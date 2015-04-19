9deb2023d554415b6e770bf11c28214e015484c2
Oliver Woodings <oliver.woodings@qubitdigital.com>
2015-04-19 19:03:10 +0100

    Fix API

--- service.js
+++ service.js
@@ -1,77 +1,80 @@
 var Waterway = require("waterway");
 var config = require("config");
 var when = require("when");
-var SocketIO = require("socket.io");
-var socketJwt = require("socketio-jwt");
+var socketIO = require("socket.io");
+var socketioJwt = require("socketio-jwt");
 var StreamManager = require("./lib/StreamManager");
 
 var service = new Waterway(config.waterway);
-var io = SocketIO(config.io.port);
+var io = socketIO(config.io.port);
 var streamManager = new StreamManager(service);
 
-io.use(socketioJwt.authorize({
-  secret: config.auth0.secret,
-  handshake: true
-}));
 
-io.on("connection", function (socket) {
+io.on("connection", socketioJwt.authorize({
+    secret: new Buffer(config.auth0.secret, "base64"),
+    timeout: 0
+  }))
+  .on("authenticated", function (socket) {
 
-  socket.on("playlist:join", function (data) {
-    if (socket.playlistId) {
-      socket.leaveAll();
+    socket.on("playlist:join", function (data) {
+      if (socket.playlistId) {
+        socket.leaveAll();
+        streamManager
+          .get(socket.playlistId)
+          .remove(socket);
+      }
+
+      var playlistId = String(data.playlistId);
+      socket.join(playlistId);
+      socket.playlistId = playlistId;
+
+      when
+        .all([
+          service.request("playlist", playlistId, "getStatus").send(),
+          service.request("playlist", playlistId, "getQueue").send()
+        ])
+        .spread(function (status, queue) {
+          socket.emit("playlist:init", {
+            status: status.data,
+            queue: queue.data
+          });
+        });
+    });
+
+    socket.on("playlist:add", function (data) {
+      service
+        .request("playlist", socket.playlistId, "add", data.trackId)
+        .send();
+    });
+
+    socket.on("playlist:upvote", function (data) {
+      service
+        .request("playlist", socket.playlistId, "upvote", data.trackId)
+        .send();
+    });
+
+    socket.on("playlist:downvote", function (data) {
+      service
+        .request("playlist", socket.playlistId, "upvote", data.trackId)
+        .send();
+    });
+
+    socket.on("playlist:play", function () {
       streamManager
         .get(socket.playlistId)
-        .remove(socket);
-    }
+        .add(socket);
+    });
 
-    var playlistId = String(data.playlistId);
-    socket.join(playlistId);
-    socket.playlistId = playlistId;
+    socket.on("disconnect", function () {
+      console.log("DC'd");
+      if (socket.playlistId) {
+        streamManager
+          .get(socket.playlistId)
+          .remove(socket);
+      }
+    });
 
-    when
-      .all([
-        service.request("playlist", playlistId, "getStatus").send(),
-        service.request("playlist", playlistId, "getQueue").send()
-      ])
-      .spread(function (status, queue) {
-        socket.emit("playlist:init", {
-          status: status.data,
-          queue: queue.data
-        });
-      });
   });
-
-  socket.on("playlist:add", function (data) {
-    service
-      .request("playlist", socket.playlistId, "add", data.trackId)
-      .send();
-  });
-
-  socket.on("playlist:upvote", function (data) {
-    service
-      .request("playlist", socket.playlistId, "upvote", data.trackId)
-      .send();
-  });
-
-  socket.on("playlist:downvote", function (data) {
-    service
-      .request("playlist", socket.playlistId, "upvote", data.trackId)
-      .send();
-  });
-
-  socket.on("playlist:play", function () {
-    streamManager
-      .get(socket.playlistId)
-      .add(socket);
-  });
-
-  socket.on("disconnect", function () {
-    streamManager
-      .get(socket.playlistId)
-      .remove(socket);
-  });
-
-});
 
 service
   .event("playlist", ":playlistId", "added", ":trackId")
