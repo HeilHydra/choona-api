2d39ff062b98f91a929254f1f33e2e0eb7681dc7
Oliver Woodings <oliver.woodings@qubitdigital.com>
2015-05-05 16:20:19 +0100

    Stop sockets from registering handlers multiple times

--- service.js
+++ service.js
@@ -9,84 +9,14 @@ var service = new Waterway(config.waterway);
 var io = socketIO(config.io.port);
 var streamManager = new StreamManager(service);
 
-
 io.on("connection", socketioJwt.authorize({
     secret: new Buffer(config.auth0.secret, "base64"),
     timeout: -1
   }))
   .on("authenticated", function (socket) {
-    socket.on("playlist:join", function (data) {
-      if (socket.playlistId) {
-        socket.leaveAll();
-        streamManager
-          .get(socket.playlistId)
-          .remove(socket);
-      }
-
-      var playlistId = String(data.playlistId);
-      socket.join(playlistId);
-      socket.playlistId = playlistId;
-
-      when
-        .all([
-          service.request("playlist", playlistId, "getStatus").send(),
-          service.request("playlist", playlistId, "getQueue").send()
-        ])
-        .spread(function (status, queue) {
-          socket.emit("playlist:init", {
-            status: status.data,
-            queue: queue.data
-          });
-        });
-    });
-
-    socket.on("playlist:add", function (trackId) {
-      service
-        .request("playlist", socket.playlistId, "add", trackId)
-        .send();
-    });
-
-    socket.on("playlist:downvote", function (trackId) {
-      service
-        .request("playlist", socket.playlistId, "downvote", trackId)
-        .send();
-    });
-
-    socket.on("playlist:upvote", function (trackId) {
-      service
-        .request("playlist", socket.playlistId, "upvote", trackId)
-        .send();
-    });
-
-    socket.on("playlist:stream:start", function () {
-      streamManager
-        .get(socket.playlistId)
-        .add(socket);
-    });
-
-    socket.on("playlist:stream:stop", function () {
-      streamManager
-        .get(socket.playlistId)
-        .add(socket);
-    });
-
-    socket.on("playlist:search", function (searchString, cb) {
-      service
-        .request("playlist", socket.playlistId, "search", searchString)
-        .send()
-        .then(function (res) {
-          cb(res.data);
-        });
-    });
-
-    socket.on("disconnect", function () {
-      if (socket.playlistId) {
-        streamManager
-          .get(socket.playlistId)
-          .remove(socket);
-      }
-    });
-
+    if (!socket.registered) {
+      registerSocket(socket);
+    }
   });
 
 service
@@ -112,3 +42,79 @@ service
   .on(function (res) {
     io.to(res.params.playlistId).emit("playlist:playing", res.data);
   });
+
+function registerSocket(socket) {
+  socket.registered = true;
+
+  socket.on("playlist:join", function (data) {
+    if (socket.playlistId) {
+      socket.leaveAll();
+      streamManager
+        .get(socket.playlistId)
+        .remove(socket);
+    }
+
+    var playlistId = String(data.playlistId);
+    socket.join(playlistId);
+    socket.playlistId = playlistId;
+
+    when
+      .all([
+        service.request("playlist", playlistId, "getStatus").send(),
+        service.request("playlist", playlistId, "getQueue").send()
+      ])
+      .spread(function (status, queue) {
+        socket.emit("playlist:init", {
+          status: status.data,
+          queue: queue.data
+        });
+      });
+  });
+
+  socket.on("playlist:add", function (trackId) {
+    service
+      .request("playlist", socket.playlistId, "add", trackId)
+      .send();
+  });
+
+  socket.on("playlist:downvote", function (trackId) {
+    service
+      .request("playlist", socket.playlistId, "downvote", trackId)
+      .send();
+  });
+
+  socket.on("playlist:upvote", function (trackId) {
+    service
+      .request("playlist", socket.playlistId, "upvote", trackId)
+      .send();
+  });
+
+  socket.on("playlist:stream:start", function () {
+    streamManager
+      .get(socket.playlistId)
+      .add(socket);
+  });
+
+  socket.on("playlist:stream:stop", function () {
+    streamManager
+      .get(socket.playlistId)
+      .add(socket);
+  });
+
+  socket.on("playlist:search", function (searchString, cb) {
+    service
+      .request("playlist", socket.playlistId, "search", searchString)
+      .send()
+      .then(function (res) {
+        cb(res.data);
+      });
+  });
+
+  socket.on("disconnect", function () {
+    if (socket.playlistId) {
+      streamManager
+        .get(socket.playlistId)
+        .remove(socket);
+    }
+  });
+}
